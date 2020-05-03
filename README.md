d24a89aa81d454e25cd0e3f807b53f5a39ed1dc8
Jsquad <lakis006@umn.edu>
2020-05-02 23:11:08 -0500

    removed some html to fit provided css, added some js, and finished local storage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # working-calendar
+
+GIVEN I am using a daily planner to create a schedule
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,26 @@
+$("button").on("click", function(){
+    var itemindex = $(this).attr("id");
+    console.log(itemindex);
+    var index = itemindex.split("-")[0];//becomes a 2 item list of whatever-whatever
+    console.log(index);
+    var userData = $("#"+index+"-input").val();
+    console.log(userData);
+    localStorage.setItem(index, userData);
+});
 
+    for (var i = 9; i < 18; i++) {
+        $("#"+i+"-input").val(localStorage.getItem(i));
+        var currentTime = moment().hours();
+        
+        if ( i < currentTime) {
+            $("#"+i+"-row").addClass("past");
+        }
+        else if (i === currentTime) {
+            $("#"+i+"-row").addClass("present");
+        }
+        else {
+            $("#"+i+"-row").addClass("future");
+        }
+    }
 
-
-
+    $("#currentDay").text(moment().format('LLLL'));
